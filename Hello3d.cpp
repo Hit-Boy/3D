834e787087344d45fe36ac9cec4bf9f0f4a48c03
Hit-Boy <sunguy2@mail.ru>
2017-02-21 12:15:56 +0300

    now i have rotation but i dont liek progrmming much
    i dont leik?

--- Hello3d.cpp
+++ Hello3d.cpp
@@ -1,19 +1,13 @@
 #include "TXLib.h"
 
-const int Zmin = -100;
 const int XWindowS = GetSystemMetrics (SM_CXSCREEN);
 const int YWindowS = GetSystemMetrics (SM_CYSCREEN);
-double ZoomX = 5000;
-double ZoomY = 5000;
-double ZoomZ = 25;
-double Vx = 0;
-double Vy = 0;
-double Vz = 0;
-double A = 0;
-double B = 0;
-double C = 0;
+double ZoomX = 50000, ZoomY = 50000, ZoomZ = 500;
+double Vx = 0, Vy = 0, Vz = 45000;
+double A = 0, B = 0, C = 0;
 
-void Line3D(int X1, int Y1, int Z1, int X2, int Y2, int Z2);
+//void Line3D(int X1, int Y1, int Z1, int X2, int Y2, int Z2);
+void Line3D (double x1, double y1, double z1, double x2, double y2, double z2);
 void Rectangle3D(int X1, int Y1, int Z1, int X2, int Y2, int Z2, COLORREF Color);
 void Scale();
 void StartAll();
@@ -26,28 +20,34 @@ int main()
     StartAll();
     }
 
-void Line3D(int X1, int Y1, int Z1, int X2, int Y2, int Z2)
+/*void Line3D(int X1, int Y1, int Z1, int X2, int Y2, int Z2)
     {
     double X1_old = X1, Y1_old = Y1, Z1_old = Z1;
     double X2_old = X2, Y2_old = Y2, Z2_old = Z2;
 
     Y1 = Z1_old * sin (A) + Y1_old * cos (A);
-    Y2 = Z2_old * sin (A) + Y2_old * cos (A);
+   Y2 = Z2_old * sin (A) + Y2_old * cos (A);
     Z1 = Z1_old * cos (A) - Y1_old * sin (A);
     Z2 = Z2_old * cos (A) - Y2_old * sin (A);
 
+    X1_old = X1;
+    X2_old = X2;
     Y1_old = Y1;
     Y2_old = Y2;
     Z1_old = Z1;
     Z2_old = Z2;
 
-    Y1 = X1_old * sin (B) + Y1_old * cos (B);
-    Y2 = X2_old * sin (B) + Y2_old * cos (B);
-    X1 = X1_old * cos (B) - Y1_old * sin (B);
+   X1 = X1_old * cos (B) - Y1_old * sin (B);
     X2 = X2_old * cos (B) - Y2_old * sin (B);
+    Y1 = X1_old * sin (B) + Y1_old * cos (B);
+   Y2 = X2_old * sin (B) + Y2_old * cos (B);
 
     X1_old = X1;
     X2_old = X2;
+    Y1_old = Y1;
+    Y2_old = Y2;
+    Z1_old = Z1;
+    Z2_old = Z2;
 
     Z1 = X1_old * sin (C) + Z1_old * cos (C);
     Z2 = X2_old * sin (C) + Z2_old * cos (C);
@@ -68,6 +68,7 @@ void Line3D(int X1, int Y1, int Z1, int X2, int Y2, int Z2)
     Y2 += Vy;
     Z2 += Vz;
 
+    const int Zmin = -100;
     txLine(X1/(Z1 - Zmin) + XWindowS/2, -Y1/(Z1 - Zmin) + YWindowS/2, X2/(Z2 - Zmin) + XWindowS/2, -Y2/(Z2 - Zmin) + YWindowS/2);
 
     X1_old = X1;
@@ -76,7 +77,61 @@ void Line3D(int X1, int Y1, int Z1, int X2, int Y2, int Z2)
     X2_old = X2;
     Y2_old = Y2;
     Z2_old = Z2;
-    }
+    }   */
+
+void Line3D (double x1, double y1, double z1, double x2, double y2, double z2)
+{
+double x1_old = x1, y1_old = y1, z1_old = z1;
+double x2_old = x2, y2_old = y2, z2_old = z2;
+
+y1 = z1_old * sin (A) + y1_old * cos (A);
+y2 = z2_old * sin (A) + y2_old * cos (A);
+z1 = z1_old * cos (A) - y1_old * sin (A);
+z2 = z2_old * cos (A) - y2_old * sin (A);
+
+y1_old = y1;
+y2_old = y2;
+z1_old = z1;
+z2_old = z2;
+
+y1 = x1_old * sin (B) + y1_old * cos (B);
+y2 = x2_old * sin (B) + y2_old * cos (B);
+x1 = x1_old * cos (B) - y1_old * sin (B);
+x2 = x2_old * cos (B) - y2_old * sin (B);
+
+x1_old = x1;
+x2_old = x2;
+
+z1 = x1_old * sin (C) + z1_old * cos (C);
+z2 = x2_old * sin (C) + z2_old * cos (C);
+x1 = x1_old * cos (C) - z1_old * sin (C);
+x2 = x2_old * cos (C) - z2_old * sin (C);
+
+x1 *= ZoomX;
+y1 *= ZoomY;
+z1 *= ZoomZ;
+
+x2 *= ZoomX;
+y2 *= ZoomY;
+z2 *= ZoomZ;
+
+x1 += Vx;
+y1 += Vy;
+z1 += Vz;
+
+x2 += Vx;
+y2 += Vy;
+z2 += Vz;
+double zmin = -100;
+txLine ((x1)/(z1 - zmin) + XWindowS / 2, YWindowS / 2 - (y1)/(z1 - zmin), XWindowS / 2 + (x2)/(z2 - zmin), YWindowS / 2 - (y2)/(z2 - zmin));
+
+x1_old = x1;
+y1_old = y1;
+z1_old = z1;
+x2_old = x2;
+y2_old = y2;
+z2_old = z2;
+}
 
 void Rectangle3D(int X1, int Y1, int Z1, int X2, int Y2, int Z2, COLORREF Color)
     {
@@ -120,19 +175,19 @@ void StartAll()
         Rotation();
         Scale();
         Movement();
-        Rectangle3D(-2, 2, 2, 2, -2, -2, RGB(55, 43, 210));
+        Rectangle3D(20, 20, 20, -20, -20, -20, RGB(55, 43, 210));
         txEnd();
         }
     }
 
 void Movement()
     {
-    if(GetAsyncKeyState('W')) Vy+= 50;
-    if(GetAsyncKeyState('A')) Vx-= 50;
-    if(GetAsyncKeyState('S')) Vy-= 50;
-    if(GetAsyncKeyState('D')) Vx+= 50;
-    if(GetAsyncKeyState('Z')) Vz+= 0.5;
-    if(GetAsyncKeyState('X')) Vz-= 0.5;
+    if(GetAsyncKeyState('W')) Vy+= 10000;
+    if(GetAsyncKeyState('A')) Vx-= 10000;
+    if(GetAsyncKeyState('S')) Vy-= 10000;
+    if(GetAsyncKeyState('D')) Vx+= 10000;
+    if(GetAsyncKeyState('Z')) Vz+= 5;
+    if(GetAsyncKeyState('X')) Vz-= 5;
     if(GetAsyncKeyState('C'))
         {
         Vx = 0;
@@ -143,12 +198,12 @@ void Movement()
 
 void Rotation()
     {
-    if (GetAsyncKeyState (VK_LEFT)) C -= 0.01;
-    if (GetAsyncKeyState (VK_RIGHT)) C += 0.01;
-    if (GetAsyncKeyState ('M')) B -= 0.01;
-    if (GetAsyncKeyState ('N')) B += 0.01;
-    if (GetAsyncKeyState (VK_UP)) A -= 0.01;
-    if (GetAsyncKeyState (VK_DOWN)) A += 0.01;
+    if (GetAsyncKeyState (VK_LEFT)) C -= 0.02;
+    if (GetAsyncKeyState (VK_RIGHT)) C += 0.02;
+    if (GetAsyncKeyState ('M')) B -= 0.02;
+    if (GetAsyncKeyState ('N')) B += 0.02;
+    if (GetAsyncKeyState (VK_UP)) A -= 0.02;
+    if (GetAsyncKeyState (VK_DOWN)) A += 0.02;
     }
 
 
